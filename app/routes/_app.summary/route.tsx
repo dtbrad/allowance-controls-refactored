bb93590a22c5b1ecbaafe8f4bef1dc415e234591
Daniel Brad <danieltbrad@gmail.com>
2024-01-05 11:53:53 -0500

    add transactions table to summary route

--- app/routes/_app.summary/route.tsx
+++ app/routes/_app.summary/route.tsx
@@ -1,12 +1,49 @@
-import {LoaderFunctionArgs} from "@remix-run/node";
+import {LoaderFunctionArgs, MetaFunction} from "@remix-run/node";
+import {useLoaderData, useSearchParams} from "@remix-run/react";
+import getTransactions from "~/db/getTransactions";
+import getUserBalance from "~/db/getUserBalance";
 import getUserFromCookie from "~/helpers-server/getUserFromCookie.server";
+import formatCurrency from "~/helpers/formatCurrency";
+import Pagination from "~/sharedComponents/Pagination";
+import TransactionsTable from "~/sharedComponents/TransactionsTable";
+import styles from "./route.module.css";
 
 export async function loader({request}: LoaderFunctionArgs) {
     const {id} = await getUserFromCookie(request);
+    const pageString = new URL(request.url).searchParams.get("page");
+    const pageNumber = pageString ? parseInt(pageString) : 1;
 
-    return null;
+    const {transactions, totalPages} = await getTransactions(id, pageNumber);
+    const balance = await getUserBalance(id);
+
+    return {transactions, balance, totalPages};
 }
 
+export const meta: MetaFunction = () => {
+    return [
+        {title: "Family Allowance - Users"},
+        {name: "description", content: "Family Allowance Summary Page"}
+    ];
+};
+
 export default function Summary() {
-    return <h1>Summary Page</h1>;
+    const data = useLoaderData<typeof loader>();
+    const [searchParams] = useSearchParams();
+    const currentPageString = searchParams.get("page");
+    const currentPage = currentPageString ? parseInt(currentPageString) : 1;
+
+    if (!data) {
+        return null;
+    }
+    const {transactions, balance, totalPages} = data;
+
+    return (
+        <div className={styles.summary}>
+            <div className={styles.header}>
+                <p>Balance: {formatCurrency(balance)}</p>
+                <Pagination currentPage={currentPage} totalPages={totalPages} />
+            </div>
+            <TransactionsTable transactions={transactions} />
+        </div>
+    );
 }
