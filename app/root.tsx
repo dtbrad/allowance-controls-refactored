dc647095501763f93ffaa223f31ce3d94fffda74
Daniel Brad <danieltbrad@gmail.com>
2024-01-04 20:31:59 -0500

    reset css

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
     Scripts,
     ScrollRestoration
 } from "@remix-run/react";
+import "./globals.css";
 
 export const links: LinksFunction = () => [
     ...(cssBundleHref ? [{rel: "stylesheet", href: cssBundleHref}] : [])
