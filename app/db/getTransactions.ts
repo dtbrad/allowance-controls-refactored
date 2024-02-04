321774c9f9a12753867ffec85d9ed87db33f2ee8
Daniel Brad <danieltbrad@gmail.com>
2024-02-04 07:26:18 -0500

    fix pagination layout shift

--- app/db/getTransactions.ts
+++ app/db/getTransactions.ts
@@ -2,6 +2,7 @@ import prisma from "~/db/prisma";
 
 export const pageSize = 12;
 
+// TODO: use raw sql to get data in one query
 export default async function getTransactions(userId: string, pageNumber = 1) {
     const total = await prisma.transaction.count({
         where: {userId: userId}
